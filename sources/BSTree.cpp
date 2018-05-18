0693b148df3bb4fdc680ee101a78c3479ad4d185
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-05-18 11:52:42 +0300

    Update BSTree.cpp

--- sources/BSTree.cpp
+++ sources/BSTree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "BSTree.h"
 
 using namespace std;
@@ -7,7 +8,7 @@ using namespace BSTree;
 
 void MenuOut()
 {
-	cout << "Выберите одну из операций:" << endl;
+	cout << "\n\nВыберите одну из операций:" << endl;
 	cout << "1. Вывести дерево на экран" << endl;
 	cout << "2. Вывести список узлов дерева:" << endl;
 	cout << "   a. Прямой обход" << endl;
@@ -18,62 +19,133 @@ void MenuOut()
 	cout << "5. Сохранить дерево в файл" << endl;
 	cout << "6. Загрузить дерево из файла" << endl;
 	cout << "7. Проверить наличие узла" << endl;
-	cout << "8. Завершение работы" << endl << endl;
+	cout << "8. Завершение работы" << endl;
+	cout << "9. Удалить дерево" << endl << endl;
+
+}
+void Menu_for_case2()
+{
+	cout << "a - Прямой обход" << endl;
+	cout << "b - Поперечный обход" << endl;
+	cout << "c - Обратный обход" << endl;
+}
+int main(int argc, char * argv[])
+{
+	setlocale(LC_ALL, "Russian");
+	Tree *tree = new Tree;
+	for (int i = 0; i < argc-1; i++)
+	{
+		cout << "Введите " << i + 1 << " элемент" << endl;
+		tree->add_element(atoi(argv[i+1]));
+	}
+	while (true)
+	{
+		MenuOut();
+		unsigned int ch;
+		cin >> ch;
+		switch (ch)
+		{
+		case 1:
+		{
+			tree->get_print_tree();
+			break;
+		}
+		case 2:
+		{
+			char ch;
+			Menu_for_case2();
+			cin >> ch;
+			if (ch == 'a')
+			{
+				tree->get_straight();
+				break;
+			}
+
+			if (ch == 'b')
+			{
+				tree->get_symmetric();
+				break;
+			}
+
+			if (ch == 'c')
+			{
+				tree->get_inverce();
+				break;
+			}
+			
+			else
+			{
+				cout << "Неправильный ввод" << endl;
+			}
+			break;
+		}
+		case 3:
+		{
+			cout << "\nВведите значение для нового узла" << endl;
+			int ins;
+			cin >> ins;
+			tree->get_insert(ins);
+			break;
+		}
+		case 4:
+		{
+			int *del = new int;
+			int *ins = new int;
+			cout << "Введите узел, который должен быть удалён" << endl;
+			cin >> *del;
+			cout << "Введите значение для нового узла" << endl;
+			cin >> *ins;
+			break;
+		}
+		case 5:
+		{
+			tree->get_write_to_file();
+			break;
+		}
+		case 6:
+		{
+			tree->get_read_from_file();
+			break;
+		}
+		case 7:
+		{
+			int data;
+			cout << "Введите узел для проверки" << endl;
+			cin >> data;
+			tree->get_check_node(data);
+			a = false;
+			break;
+		}
+		case 8:
+		{
+			string *exit = new string;
+			cout << "Вы хотите выйти?" << endl;
+			cin >> *exit;
+			if ((*exit == "y") || (*exit == "Y") || (*exit == "yes") || (*exit == "Yes") || (*exit == "YES"))
+			{
+				return 0;
+			}
+
+			if ((*exit == "n") || (*exit == "N") || (*exit == "no") || (*exit == "No") || (*exit == "NO"))
+			{
+				cout << endl;
+				break;
+			}
+
+			else
+			{
+				cout << "Неверный ввод" << endl << endl;
+			}
+			delete exit;
+			break;
+		}
+		default:
+		{
+			cout << "Неверный ввод" << endl << endl;
+			return 0;
+		}
+		tree->get_deltree();
+		}
+	}
 	
-}
-
-void Tree::addnode(int data, Node *&pNTree) //Функция добавления узла в дерево
-{
-	if (pNTree == nullptr) //Если узлов дерева нет
-	{
-		pNTree = new Node; //Создаём новый узел
-		pNTree->data = data; //Записываем туда значения
-		pNTree->pLeft = nullptr; //Делаем пустые указатели от этого узла налево и направо
-		pNTree->pRight = nullptr;
-	}
-
-	if (data < pNTree->data) //Если добавленный элемент меньше текущего узла     
-	{
-		if (pNTree->pLeft != nullptr) // Если узел НЕ содержит пустого указателя налево
-		{
-			addnode(data, pNTree->pLeft); //Добавляем данные и указатель на левый узел
-		}
-		else
-		{
-			pNTree->pLeft = new Node; //Иначе делаем новый узел
-			pNTree->pLeft->pLeft = nullptr; //Левую ветвь левого элемента
-			pNTree->pLeft->pRight = nullptr; //Правую ветвь левого элемента
-			pNTree->pLeft->data = data; //Записываем данные в левый узел
-		}
-	}
-
-
-	if (data > pNTree->data) //Если больше текущего узла
-	{ //Всё то же самое, на левую ветвь
-		if (pNTree->pRight != nullptr)
-		{
-			addnode(data, pNTree->pRight);
-		}
-		else
-		{
-			pNTree->pRight = new Node;
-			pNTree->pRight->pRight = nullptr;
-			pNTree->pRight->pLeft = nullptr;
-			pNTree->pRight->data = data;
-		}
-	}
-}
-
-void Tree::addel(int data) //Функция добавления элемента в узел
-{
-	addnode(data, proot);
-}
-
-void Tree::print_tree(Node *&pNTree)
-{
-	if (proot == nullptr)
-	{
-		std::cout << "Дерево пусто";
-	}
-	std::cout << "--" << pNTree->data;
 }
